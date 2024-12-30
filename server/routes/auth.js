fee25bf9e1f1f7f8a53b3e4137a6968e950211e3
SharifAly <sharif.aly_@outlook.com>
2024-12-30 21:50:40 +0100

    change cookie options

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -58,8 +58,7 @@ router.post("/login", async (req, res) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "none", // Erlaubt Cross-Site-Cookies
-        domain: "blog.sharif-aly.tech", // Domain, auf der das Cookie verfügbar ist
+        domain: "https://blog.sharif-aly.tech/", // Domain, auf der das Cookie verfügbar ist
         path: "/", // Cookie auf allen Pfaden verfügbar machen
         maxAge: 10 * 60 * 60 * 1000, // 10 hours
       });
