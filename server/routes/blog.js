4b938d0916b345a8a6b73b1b07bb89a17fddc141
SharifAly <sharif.aly_@outlook.com>
2024-12-30 21:34:25 +0100

    set debug ptions

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -33,7 +33,7 @@ router.post("/post", (req, res) => {
   const { title, category, image, body } = req.body; // Get post details from request body
   const sql =
     "INSERT INTO blog.posts (title, category, image, body, fk_user_id) VALUES (?,?,?,?,?)"; // SQL query to insert a new post
-  const token = req.cookies.token; // Get JWT token from cookies
+  const token = req.cookies; // Get JWT token from cookies
   console.log("1", token);
 
   const decode = jwt.verify(token, process.env.JWT_SECRET_KEY); // Verify and decode the token
